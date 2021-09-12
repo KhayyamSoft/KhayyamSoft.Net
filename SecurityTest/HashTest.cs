d0eb23da5dd8f6c209d6991f528f32eec95a5cc5
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2021-09-12 11:16:44 +0430

    Added Test Class For Hash

---
+++ SecurityTest/HashTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Security.Hash;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecurityTest
+{
+    [TestClass]
+    public class HashTest
+    {
+        [TestMethod]
+        public void HashNullInputTest() 
+        {
+            var test = Hash.GetSHA256Hash("");
+            Assert.IsNull(test);
+        }
+    }
+}
