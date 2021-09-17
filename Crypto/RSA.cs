f839498adacfd9321a3b36e74545553cc388b158
Siyavash Feyzi <58208265+siyavashfeyzi@users.noreply.github.com>
2021-09-17 21:46:41 +0430

    Added RSA Class

---
+++ Crypto/RSA.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Security.Crypto
+{
+    /// <summary>
+    /// Providing methods to encrypt and decrypt data using Advanced Encryption Standard (RSA) / Rijndael 
+    /// </summary>
+    public class RSA
+    {
+    }
+}
