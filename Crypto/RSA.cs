5e72a9ea9d5757613ed730ba197a29a69ac59c69
AliSepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2021-09-21 12:31:19 +0430

    Crypto.RSA Cleanup Code

--- Crypto/RSA.cs
+++ Crypto/RSA.cs
@@ -1,105 +1,80 @@
-﻿using System;
-using System.Security.Cryptography;
+﻿using System.Security.Cryptography;
 using System.Text;
 
 namespace Security.Crypto
 {
     /// <summary>
-    /// Providing methods to encrypt and decrypt data using Advanced Encryption Standard (RSA) / Rijndael 
-    /// more details:
+    /// Providing methods to encrypt and decrypt data using Advanced Encryption Standard (RSA) / Rijndael. <br/>
     ///  The following code uses the RSACryptoServiceProvider class
     ///  to encrypt a string into an array of bytes and then decrypt the bytes back into a string.
     /// </summary>
     public static class RSA
     {
         /// <summary>
-        ///  Pass the data to ENCRYPT, the public key information 
-        /// (using RSACryptoServiceProvider.ExportParameters(false),
-        /// and a boolean flag specifying no OAEP padding.
+        ///  Pass the data to ENCRYPT and the public key information 
+        /// (using RSACryptoServiceProvider.ExportParameters(false) <br/>
         /// example for use this method:
-        /// encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false), false);
+        /// encryptedData = RSAEncrypt(dataToEncrypt, RSA.ExportParameters(false));
         /// </summary>
         /// <param name="input">Data to encrypt</param>
-        /// <param name="RSAKeyInfo">Key to encrypt. RSAKeyInfo is an byte Array</param>
-        /// <param name="DoOAEPPadding"></param>
+        /// <param name="rsaKeyInfo">RSAKeyInfo (Encrypt key)</param>
+        /// <param name="doOAEPPadding"></param>
         /// <returns>Encrypted byte array</returns>
-        public static byte[] RSAEncrypt(string input, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        public static byte[] RSAEncrypt(string input, RSAParameters rsaKeyInfo, bool doOAEPPadding = false)
         {
             //Create a UnicodeEncoder to convert between byte array and string.
-            var ByteConverter = new UnicodeEncoding();
+            var byteConverter = new UnicodeEncoding();
 
             //Create byte arrays to hold original, encrypted data.
-            var dataToEncrypt = ByteConverter.GetBytes(input);
+            var dataToEncrypt = byteConverter.GetBytes(input);
 
-            try
+            byte[] encryptedData;
+            //Create a new instance of RSACryptoServiceProvider.
+            using (var rsa = new RSACryptoServiceProvider())
             {
-                byte[] encryptedData;
-                //Create a new instance of RSACryptoServiceProvider.
-                using (var RSA = new RSACryptoServiceProvider())
-                {
-                    //Import the RSA Key information. This only needs
-                    //toinclude the public key information.
-                    RSA.ImportParameters(RSAKeyInfo);
+                //Import the RSA Key information. This only needs
+                //toinclude the public key information.
+                rsa.ImportParameters(rsaKeyInfo);
 
-                    //Encrypt the passed byte array and specify OAEP padding.  
-                    //OAEP padding is only available on Microsoft Windows XP or
-                    //later.  
-                    encryptedData = RSA.Encrypt(dataToEncrypt, DoOAEPPadding);
-                }
-                return encryptedData;
+                //Encrypt the passed byte array and specify OAEP padding.  
+                //OAEP padding is only available on Microsoft Windows XP or
+                //later.  
+                encryptedData = rsa.Encrypt(dataToEncrypt, doOAEPPadding);
             }
-             //Catch and display a CryptographicException  
-             //to the console.
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.Message);
-                return null;
-            }
-
+            return encryptedData;
         }
 
         /// <summary>
-        /// Pass the data to DECRYPT, the private key information 
-        /// (using RSACryptoServiceProvider.ExportParameters(true),
-        /// and a boolean flag specifying no OAEP padding.
+        /// Pass the data to DECRYPT and the private key information 
+        /// (using RSACryptoServiceProvider.ExportParameters(true)
         /// example for use this method:
-        /// decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true), false);
+        /// decryptedData = RSADecrypt(encryptedData, RSA.ExportParameters(true));
         /// </summary>
         /// <param name="input">Data to encrypt</param>
-        /// <param name="RSAKeyInfo">Key to decrypt. RSAKeyInfo is an byte Array</param>
-        /// <param name="DoOAEPPadding">Decrypt byte array</param>
-        public static byte[] RSADecrypt(string input, RSAParameters RSAKeyInfo, bool DoOAEPPadding)
+        /// <param name="rsaKeyInfo">RSAKeyInfo (Decrypt key)</param>
+        /// <param name="doOAEPPadding">Decrypt byte array</param>
+        public static byte[] RSADecrypt(string input, RSAParameters rsaKeyInfo, bool doOAEPPadding = false)
         {
             //Create a UnicodeEncoder to convert between byte array and string.
-            var ByteConverter = new UnicodeEncoding();
+            var byteConverter = new UnicodeEncoding();
 
             //Create byte arrays to hold original, decrypt data.
-            var dataToDecrypt = ByteConverter.GetBytes(input);
+            var dataToDecrypt = byteConverter.GetBytes(input);
 
-            try
+            byte[] decryptedData;
+            //Create a new instance of RSACryptoServiceProvider.
+            using (var rsa = new RSACryptoServiceProvider())
             {
-                byte[] decryptedData;
-                //Create a new instance of RSACryptoServiceProvider.
-                using (var RSA = new RSACryptoServiceProvider())
-                {
-                    //Import the RSA Key information. This needs
-                    //to include the private key information.
-                    RSA.ImportParameters(RSAKeyInfo);
+                //Import the RSA Key information. This needs
+                //to include the private key information.
+                rsa.ImportParameters(rsaKeyInfo);
 
-                    //Decrypt the passed byte array and specify OAEP padding.  
-                    //OAEP padding is only available on Microsoft Windows XP or
-                    //later.  
-                    decryptedData = RSA.Decrypt(dataToDecrypt, DoOAEPPadding);
-                }
-                return decryptedData;
+                //Decrypt the passed byte array and specify OAEP padding.  
+                //OAEP padding is only available on Microsoft Windows XP or
+                //later.  
+                decryptedData = rsa.Decrypt(dataToDecrypt, doOAEPPadding);
             }
-            //Catch and display a CryptographicException  
-            //to the console.
-            catch (CryptographicException e)
-            {
-                Console.WriteLine(e.ToString());
-                return null;
-            }
+            return decryptedData;
         }
 
     }
