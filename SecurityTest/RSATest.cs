100b107dc583312e6da19dac725da012bfdbcf11
AliSepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2021-09-21 12:31:39 +0430

    RSA Simple Test

---
+++ SecurityTest/RSATest.cs
@@ -0,0 +1,27 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RSA = Security.Crypto.RSA;
+
+namespace SecurityTest
+{
+    [TestClass]
+    public class RSATest
+    {
+        [TestMethod]
+        public void EncryptDecryptTest()
+        {
+            using (var rsa = new RSACryptoServiceProvider())
+            {
+                const string testString = "Hello World!";
+                var byteConverter = new UnicodeEncoding();
+                var key = rsa.ExportParameters(true);
+                var encryptedBytes = RSA.RSAEncrypt(testString, rsa.ExportParameters(false));
+                var encryptedString = byteConverter.GetString(encryptedBytes);
+                var decryptedBytes = RSA.RSADecrypt(encryptedString, rsa.ExportParameters(true));
+                var decryptedString = byteConverter.GetString(decryptedBytes);
+                Assert.IsTrue(decryptedString == testString);
+            }
+        }
+    }
+}
