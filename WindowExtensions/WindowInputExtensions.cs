88755630a4cab3070dabf984ecf707722348f996
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-13 00:36:40 +0430

    Window Drag Extension

---
+++ WindowExtensions/WindowInputExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows.Input;
+using System.Windows;
+
+namespace AS.Windows.WindowExtensions
+{
+	public static class WindowInputExtensions
+	{
+		/// <summary>
+		/// Make Windows Moving With Mouse Drag Until Left Button Is Released (Use On Mouse Down Event Handler)
+		/// </summary>
+		/// <param name="restrictMaximizeDrag">Set True If You Dont Want Maximized Window Being Dragged! Default Is True.</param>
+		public static void DragWindowWithMouse(this Window window, MouseButtonEventArgs e, bool restrictMaximizeDrag = true)
+		{
+			if (restrictMaximizeDrag && window.WindowState != WindowState.Normal) return;
+			while (e.LeftButton == MouseButtonState.Pressed) window.DragMove();
+		}
+	}
+}
