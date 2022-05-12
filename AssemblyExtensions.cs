c8d4c06b6c315be43178ded25064e6677bc948b4
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-13 00:35:37 +0430

    GetVersion() For AssemblyExtensions

---
+++ AssemblyExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Reflection;
+
+namespace AS.Windows
+{
+	public static class AssemblyExtensions
+	{
+		public static Version GetVersion(this Assembly assembly) => assembly.GetName().Version;
+		public static string GetVersion(this Assembly assembly, int fieldCount = 2) => assembly.GetVersion().ToString(fieldCount);
+	}
+}
