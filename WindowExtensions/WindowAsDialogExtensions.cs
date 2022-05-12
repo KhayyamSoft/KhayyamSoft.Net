8b964a17c5c814bfea4b1b247d42cecc2c70a8a8
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-13 00:37:08 +0430

    SafeCloseDialog Extension For Windows

---
+++ WindowExtensions/WindowAsDialogExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace AS.Windows.WindowExtensions
+{
+	public static class WindowAsDialogExtensions
+	{
+		/// <summary>
+		/// Sets Dialog Result And Close It (Without Exception)
+		/// </summary>
+		/// <param name="window">Dialog To Close</param>
+		/// <param name="dialogResult">Result To Set As Dialog Result</param>
+		/// <returns></returns>
+		public static bool SafeCloseDialog(this System.Windows.Window window, bool? dialogResult = null)
+		{
+			try { window.DialogResult = true; } catch { return false; }
+			try { window.Close(); } catch { return false; }
+			return true;
+		}
+	}
+}
