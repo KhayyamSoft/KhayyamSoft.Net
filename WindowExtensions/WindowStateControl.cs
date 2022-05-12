8d1ff49b7ae003df48a3576389bbd52c65b4b107
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-13 00:36:22 +0430

    Window State Extensions (Max,Min,Normal)

---
+++ WindowExtensions/WindowStateControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace AS.Windows.WindowExtensions
+{
+	public static class WindowStateControl
+	{
+		/// <summary>
+		/// Change Window State Between Normal And Maximized
+		/// </summary>
+		public static void SwitchWindowState(this Window window)
+			=> window.WindowState = window.WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
+
+		public static void Minimize(this Window window) => window.WindowState = WindowState.Minimized;
+
+		public static void Maximize(this Window window) => window.WindowState = WindowState.Maximized;
+
+		/// <summary>
+		/// Make Windows State Normal
+		/// </summary>
+		public static void Normalize(this Window window) => window.WindowState = WindowState.Normal;
+	}
+}
