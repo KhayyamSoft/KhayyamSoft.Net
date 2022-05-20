034f0d7b1b54186afd72b3b75b733867d6f19b8f
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-20 23:54:26 +0430

    Merge AS.Security.BC Into This Project

---
+++ BC/BCBase.cs
@@ -0,0 +1,49 @@
+﻿using Org.BouncyCastle.Crypto.Paddings;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Crypto;
+using System;
+
+namespace Security.BC
+{
+	public static class BCBase
+	{
+		/// <summary>
+		/// Encrypt/Decrypt Bytes Sequence With A Specified Key And BouncyCastle Crypto Engine
+		/// </summary>
+		/// <param name="forEncrypt">Set true For Encrypt And false For Decrypt</param>
+		/// <param name="input">Bytes Sequence To Encrypt/Decrypt</param>
+		/// <param name="keyByte">Encrypt/Decrypt Key</param>
+		/// <param name="blockCipher">Use One Engine From Org.BouncyCastle.Crypto.Engines</param>
+		/// <returns></returns>
+		private static byte[] EncryptOrDecrypt(bool forEncrypt, byte[] input,
+			 ReadOnlySpan<byte> keyByte, IBlockCipher blockCipher)
+		{
+			var cipher = new PaddedBufferedBlockCipher(blockCipher);
+			cipher.Init(forEncrypt, new KeyParameter(keyByte.ToArray()));
+			var result = cipher.DoFinal(input);
+			//SAFETY
+			cipher = null;
+			return result;
+		}
+
+		/// <summary>
+		/// Encrypt Bytes With Specified Key And BouncyCastle Crypto Engine
+		/// </summary>
+		/// <param name="plain">Unencrypted Bytes</param>
+		/// <param name="keyByte">Key For Encryption</param>
+		/// <param name="blockCipher">Use One Engine From Org.BouncyCastle.Crypto.Engines</param>
+		/// <returns>Encrypted Bytes</returns>
+		public static byte[] Encrypt(byte[] plain, ReadOnlySpan<byte> keyByte, IBlockCipher blockCipher)
+			=> EncryptOrDecrypt(true, plain, keyByte, blockCipher);
+
+		/// <summary>
+		/// Decrypt Base64 Encoded String With Specified Key And BouncyCastle Crypto Engine
+		/// </summary>
+		/// <param name="cipher">Base64 Encoded String</param>
+		/// <param name="keyByte">Key For Decryption</param>
+		/// <param name="blockCipher">Use One Engine From Org.BouncyCastle.Crypto.Engines</param>
+		/// <returns>Decrypted Bytes</returns>
+		public static byte[] Decrypt(string cipher, ReadOnlySpan<byte> keyByte, IBlockCipher blockCipher)
+			=> EncryptOrDecrypt(false, Convert.FromBase64String(cipher), keyByte, blockCipher);
+	}
+}
---
+++ BC/DataHelper.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Security.BC
+{
+	internal static class DataHelper
+	{
+		internal static Span<byte> CharSpanToByteSpan(ReadOnlySpan<char> key, Encoding encoding)
+		{
+			var byteCount = encoding.GetByteCount(key);
+			Span<byte> keyByte = new Span<byte>(new byte[byteCount]);
+			encoding.GetBytes(key, keyByte);
+			return keyByte;
+		}
+	}
+}
