3cd73da49c24632e8661c97efe80f6d5a363c7c3
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-21 11:12:35 +0430

    Thread Extensions For Culture Set

---
+++ ThreadExtensions/ThreadCultureExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using System.Threading;
+
+namespace KhayyamApps.Windows.ThreadExtensions
+{
+	public static class ThreadCultureExtensions
+	{
+		/// <summary>
+		/// Set Thread CurrentCulture & CurrentUICulture
+		/// </summary>
+		public static void SetThreadCulture(this Thread thread, CultureInfo cultureInfo)
+		{
+			thread.CurrentCulture = cultureInfo;
+			thread.CurrentUICulture = cultureInfo;
+		}
+
+		/// <summary>
+		/// Set Thread CurrentCulture & CurrentUICulture To Invariant Culture (new CultureInfo(string.Empty))
+		/// </summary>
+		public static void SetThreadCulture(this Thread thread) => SetThreadCulture(thread, new CultureInfo(string.Empty));
+	}
+}
