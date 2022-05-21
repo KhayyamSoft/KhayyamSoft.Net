9d6b2ad306b0eba358e91dab6fc7f5dfa7ff6171
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-05-21 17:22:57 +0430

    Application Extensions To Work With Resource Dictionaries And Theming

---
+++ CollectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace KhayyamApps.Windows
+{
+	public static class CollectionExtensions
+	{
+		public static void AddRange<T>(this Collection<T> collection, IEnumerable<T> items)
+		{ foreach (var i in items) collection.Add(i); }
+	}
+}
