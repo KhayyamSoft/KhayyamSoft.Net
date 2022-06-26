27565f98cb965ca7a8974c1a6688e16885ce74b6
alisepehrnia <41667555+alisepehrnia@users.noreply.github.com>
2022-06-26 10:04:17 +0430

    Improve Bool2VisibilityConverter

--- DataConverters/Bool2VisibilityConverter.cs
+++ DataConverters/Bool2VisibilityConverter.cs
@@ -7,15 +7,29 @@ namespace KhayyamApps.Windows.DataConverters
 {
 	/// <summary>
 	/// Converts True To Visibility.Visible And False To Visibility.Collapsed
-	/// And Vice Versa
+	/// And Vice Versa.
+	/// If Convert Fails Returns Visibility.Collapsed
 	/// </summary>
 	[ValueConversion(typeof(bool), typeof(Visibility))]
 	public class Bool2VisibilityConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-			=> (bool)value == true ? Visibility.Visible : Visibility.Collapsed;
+		{
+			//If Convert Fails Return Visibility.Collapsed
+			if (!bool.TryParse(value.ToString(), out bool result))
+			{
+				return Visibility.Collapsed;
+			}
+			return result == true ? Visibility.Visible : Visibility.Collapsed;
+		}
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-			=> (int)value <= 0 ? Visibility.Visible : Visibility.Collapsed;
+		{
+			if (!int.TryParse(value.ToString(), out int result))
+			{
+				return Visibility.Collapsed;
+			}
+			return result <= 0 ? Visibility.Visible : Visibility.Collapsed;
+		}
 	}
 }
